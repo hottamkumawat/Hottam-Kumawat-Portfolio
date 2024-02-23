154591bb9257030966e07c80086b8613df6971c5
Hottam Kumawat <hottamkumawat@gmail.com>
2024-02-23 20:51:59 +0530

    Added some changer in navbar and js of portfolio

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function toggleMenu() {
     var ul = document.querySelector('#nav_bar ul');
     ul.style.display = ul.style.display === 'none' ? 'flex' : 'none';
 }
+
+document.addEventListener("contextmenu", function(e){
+    e.preventDefault()
+},false)
